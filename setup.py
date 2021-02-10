2dddc7d707e376a7174b32891153d0917f80d79b
bdatto <bob.dattore@gmail.com>
2021-02-10 13:46:47 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mysci", 
+    version="1.0.0",
+    description="A sample package",
+    author="Bob Dattore",
+    author_email="dattore@ucar.edu",
+    packages=["mysci"],
+    install_requires=[])
